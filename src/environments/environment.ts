08efc9ac6fdab3b04211a2f1f8ab19a6589d0ed9
axelfoxII <asfoxii@gmail.com>
2022-06-12 01:31:44 -0400

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'deteccionfaceapi',
+    appId: '1:781661341219:web:8df7050e19a65937ad5396',
+    storageBucket: 'deteccionfaceapi.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyBm4bGtmGv7NATAPKF5Ql1gQxEOh5wDTXQ',
+    authDomain: 'deteccionfaceapi.firebaseapp.com',
+    messagingSenderId: '781661341219',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
