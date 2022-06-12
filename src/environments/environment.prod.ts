082c134beb8eaf33d5eed38c03a321977da600a0
axelfoxII <asfoxII@gmail.com>
2022-06-12 01:34:19 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
-  firebase: {
-    projectId: 'deteccionfaceapi',
-    appId: '1:781661341219:web:8df7050e19a65937ad5396',
-    storageBucket: 'deteccionfaceapi.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyBm4bGtmGv7NATAPKF5Ql1gQxEOh5wDTXQ',
-    authDomain: 'deteccionfaceapi.firebaseapp.com',
-    messagingSenderId: '781661341219',
-  },
+  
+  //CREAR SU PROYECTO EN FIREBASE
+  
   production: true
 };
